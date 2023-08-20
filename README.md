f29393cd903e44b11025dba268a8d03d9888c737
kafrawi <hassaballahelkafrawi@gmail.com>
2023-08-21 01:28:26 +0300

    added readme file

---
+++ README.md
@@ -0,0 +1,10 @@
+### Telegram Scraper
+
+## Guide
+
+1. Make sure to create an account on https://my.telegram.org/auth 
+2. follow the steps on https://core.telegram.org/api/obtaining_api_id to get API ID and API hash
+3. create your config file 
+4. adjust variable telegram_channel with the channel your wish to scrape
+5. adjust start date in the loop
+6. run the script
--- main.py
+++ main.py
@@ -8,13 +8,13 @@ config = ConfigParser()
 config.read(config_file)
 
 api_id, api_hash = config['account']['api_id'], config['account']['api_hash']
-telegram_channel = config['account']['tele_channel']
+telegram_channel = config['account']['tele_channel'] #place telegram channel here
 
 
 df = pd.DataFrame()
 
 with TelegramClient('scrape',api_id,api_hash) as client:
-    for message in client.iter_messages(telegram_channel,offset_date=datetime.date(2021,1,1), reverse=True):
+    for message in client.iter_messages(telegram_channel,offset_date=datetime.date(2021,1,1), reverse=True): #choose start date 
         data = {"text": message.text,
                 "date":message.date}
         temp_df = pd.DataFrame(data, index=[1])
